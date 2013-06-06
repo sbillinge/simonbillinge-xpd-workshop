8020130185f1ffcf62fc1c50716cbcbb86a95240
sbillinge <simon.billinge@gmail.com>
2013-06-06 16:18:37 -0400

    expanded docstring for quadraticroots

--- py03import/ex2math/quadraticroots.py
+++ py03import/ex2math/quadraticroots.py
@@ -8,8 +8,22 @@ import cmath
 
 
 def solveQuadraticRoots(A, B, C):
-    # FIXME: convert x1, x2 from complex to float type if the roots are real.
-    """Solve roots of a quadratic polynomial A*x**2 + B*x + C
+    """Find roots of a quadratic polynomial A*x**2 + B*x + C
+    
+    Parameters
+    ----------
+    arglist:
+    A: float-like
+       coefficient of x**2 term
+    B: float-like
+       coefficient of x term
+    C: float-like
+       constant
+    
+    Returns
+    -------
+    two roots of the equation, x1 and x2
+    
     """
     det = cmath.sqrt(B**2 - 4 * A * C)
     x1 = (-B + det) / (2 * A)
