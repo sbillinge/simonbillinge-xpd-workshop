f2639da47b8e0d3d461a083e32d4c8d65a4d05ad
sbillinge <simon.billinge@gmail.com>
2013-06-06 11:56:04 -0400

    simon's attempt at sort-last function

--- py01basic/list1.py
+++ py01basic/list1.py
@@ -45,8 +45,11 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return
+    
+    def return_last(t):
+        return t[-1]
+       
+    return sorted(tuples, key=return_last)
 
 
 # Simple provided test() function used in main() to print
